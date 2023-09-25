656ea0433464465e6242a12a8cb3797cf70688ff
karadenizemirr <karadenizemirr@gmail.com>
2023-09-25 10:22:52 +0300

    User Function

---
+++ src/database/models/User.ts
@@ -0,0 +1,26 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from "typeorm";
+import {v4 as uuid4} from 'uuid'
+
+@Entity()
+export class User{
+    @PrimaryGeneratedColumn('uuid')
+    id: string = uuid4();
+
+    @Column()
+    name: string;
+
+    @Column()
+    surname: string
+
+    @Column()
+    email: string
+
+    @Column()
+    phone:string
+
+    @Column()
+    password: string
+
+    @CreateDateColumn()
+    created_at: Date
+} 
