aa6d51a9e2dfca94595b39b8bc8839328ffaf08f
karadenizemirr <karadenizemirr@gmail.com>
2023-09-28 17:03:07 +0300

    Database Update

--- src/database/postgresql/postgresql.provider.ts
+++ src/database/postgresql/postgresql.provider.ts
@@ -10,37 +10,17 @@ import { Product } from "../models/product/Product"
 import { User } from "../models/User"
 import { Visitor } from "../models/Visitor"
 
-// export const postgresqlProviders = [
-//     {
-//         provide: 'DATA_SOURCE',
-//         useFactory: () => {
-//             const dataSource = new DataSource({
-//                 type: 'mysql',
-//                 host: 'uzb4o9e2oe257glt.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-//                 username: 'e2l9dtmizzsg8a7b',
-//                 password: 'vxdj4h3wkyweoya7',
-//                 database: 'c8tofqdid2iy4zqg',
-//                 port: 3306,
-//                 entities: [Category, Language, Amenitlies, Description, Detail, Location, Media, Product, User, Visitor],
-//                 synchronize: true
-//             })
-//             return dataSource.initialize()
-            
-//         }
-//     }
-// ]
-
 export const postgresqlProviders = [
     {
         provide: 'DATA_SOURCE',
         useFactory: () => {
             const dataSource = new DataSource({
-                type: 'postgres',
-                host: 'localhost',
-                username: 'postgres',
-                password: '123456',
-                database: 'cemreozoren',
-                port: 5432,
+                type: 'mysql',
+                host: 'uzb4o9e2oe257glt.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+                username: 'e2l9dtmizzsg8a7b',
+                password: 'vxdj4h3wkyweoya7',
+                database: 'c8tofqdid2iy4zqg',
+                port: 3306,
                 entities: [Category, Language, Amenitlies, Description, Detail, Location, Media, Product, User, Visitor],
                 synchronize: true
             })
